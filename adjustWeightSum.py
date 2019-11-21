39c47e65cd7c2b74cdbb9cde3d13268791c15829
Roxanne Miller <roxanne@Roxannes-MacBook-Pro.local>
2019-11-20 20:37:19 -0800

    adjusted weighted sum

---
+++ adjustWeightSum.py
@@ -0,0 +1,80 @@
+#CSC466 F19 Lab 6
+#Sarah Bae, shbae@calpoly.edu
+#Roxanne Miller, rmille60@calpoly.edu
+#weightedSum.py: python3 weightedSum.py <userid> <itemid> <output csv filename>
+
+import sys
+import parser
+import numpy as np
+
+def cosSim(doc1, doc2):
+    numer = 0
+    denom1 = 0
+    denom2 = 0
+
+    numerArray = np.multiply(doc1, doc2)
+    numer = np.sum(numerArray)
+
+    denom1Array = np.power(doc1, 2)
+    denom1 = np.sum(denom1Array)
+    denom1 = np.sqrt(denom1)
+
+    denom2Array = np.power(doc2, 2)
+    denom2 = np.sum(denom2Array)
+    denom2 = np.sqrt(denom2)
+
+    return numer / (denom1 * denom2)
+
+def userAvgRating(user):
+    return np.mean(user.ratings)
+
+def compareUsers(targetUser, secondUser, itemId):
+    targetUserItems = targetUser.ratedItems[targetUser.ratedItems != itemId]
+    matchingItems = np.intersect1d(targetUserItems, secondUser.ratedItems)
+   
+    targetUserIndices = np.searchsorted(targetUserItems, matchingItems)
+    secondUserIndices = np.searchsorted(secondUser.ratedItems, matchingItems)    
+    
+    targetUserRatings = np.take(targetUser.ratings, targetUserIndices)
+    secondUserRatings = np.take(secondUser.ratings, secondUserIndices)
+
+    return targetUserRatings, secondUserRatings
+
+def weightedSum(userId, itemId, users, items):
+    summation = 0
+    normalFactor = 0
+    targetUser = users[userId]
+    targetItem = items[itemId]
+    
+    for i in range(len(users)):
+        if i == userId or itemId not in users[i].ratedItems:
+            continue
+        else:
+            secondUser = users[i]
+            targetUserAvg = userAvgRating(targetUser)
+            targetUserRatings, secondUserRatings = compareUsers(targetUser, secondUser, itemId)
+            similarity = cosSim(targetUserRatings, secondUserRatings)
+            
+            secondUserUtilityIndex, = np.where(secondUser.ratedItems == itemId)
+            secondUserUtility = secondUser.ratings[secondUserUtilityIndex] - userAvgRating(secondUser)
+            
+            summation += similarity * secondUserUtility
+            normalFactor += abs(similarity)
+
+    return targetUserAvg + ((1 / normalFactor) * summation)
+
+def main():
+    jokeCsv = './jester-data-1.csv'
+    completeRatingsMatrix, users, items = parser.toSparseMatrix(jokeCsv)
+    # print(completeRatingsMatrix)
+    userId = int(sys.argv[1])
+    itemId = int(sys.argv[2])
+
+    outfile = sys.argv[3]
+
+    prediction = weightedSum(userId, itemId, users, items)
+    print(prediction)
+
+
+if __name__ == '__main__':
+    main()
